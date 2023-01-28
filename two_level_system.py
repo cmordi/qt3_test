3a4fbd0ec5d113b3945aa25ec7300c02973771cb
princezukoburner <king.mordi.com@gmail.com>
2023-01-27 18:09:18 -0800

    testing push

---
+++ two_level_system.py
@@ -0,0 +1,11 @@
+import qutip as qtp
+
+def hamiltonian(rabi_f):
+    
+    """This returns the hamiltonian"""
+    
+    return "this is supposed to be my hamiltonian equation"
+
+def new_function(x):
+    
+    return 25 * x
